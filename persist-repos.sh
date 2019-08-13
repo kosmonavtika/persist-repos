d4d9a6d075be0357328bb1e37ead962623f43032
Dom <leancode@users.noreply.github.com>
2019-08-13 11:30:19 +0200

    Compatibility with Proxmox 6

--- persist-repos.sh
+++ persist-repos.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 function persist_repos() {
-  ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
-  FREE_REPO_LIST="/etc/apt/sources.list.d/pve-no-subscription.list"
-  FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve stretch pve-no-subscription"
-  echo $FREE_REPO_LINE > $FREE_REPO_LIST
-  rm -f $ENTERPRISE_REPO_LIST
+
+  ENTERPRISE_REPO_FILE="/etc/apt/sources.list.d/pve-enterprise.list"
+  ENTERPRISE_REPO_LINE="# deb https://enterprise.proxmox.com/debian/pve buster pve-enterprise"
+  echo $ENTERPRISE_REPO_LINE > $ENTERPRISE_REPO_FILE
+
+  FREE_REPO_FILE="/etc/apt/sources.list.d/pve-no-subscription.list"
+  FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve buster pve-no-subscription"
+  echo $FREE_REPO_LINE > $FREE_REPO_FILE
+
   sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+  sed -i.bak "s/data.status === 'Active'/true/g" /usr/share/pve-manager/js/pvemanagerlib.js
   echo "$0: Updated repositories..."
 }
 
