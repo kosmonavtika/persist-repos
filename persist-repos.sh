a45288e0f44e7fd900252f27cdbd7263fb95d6b1
kosmonavtika <jimmy@vostok.dev>
2019-08-21 14:37:06 +0200

    add support for proxmox 6.0 (buster)

--- persist-repos.sh
+++ persist-repos.sh
@@ -3,10 +3,10 @@
 function persist_repos() {
   ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
   FREE_REPO_LIST="/etc/apt/sources.list.d/pve-no-subscription.list"
-  FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve stretch pve-no-subscription"
+  FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve buster pve-no-subscription"
   echo $FREE_REPO_LINE > $FREE_REPO_LIST
   rm -f $ENTERPRISE_REPO_LIST
-  sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+  sed -i.bak "s/data.status === 'Active'/true/g" /usr/share/pve-manager/js/pvemanagerlib.js
   echo "$0: Updated repositories..."
 }
 
